ddf3e619d94da7ba5b825a2adb613c71bbf852bb
ysdexlic <david.arthur.thompson@gmail.com>
2017-08-27 12:13:02 +0100

    - fixing server for /jumping-letters endpoint - updating demo to 1.2.0

--- demo/server.js
+++ demo/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/build'));
+app.use('/jumping-letters', express.static(__dirname + '/build'));
 
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, './build/index.html'));
